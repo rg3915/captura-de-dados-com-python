1af5d194fc86742c344b4d2527ae58e8a0920c0d
rg3915 <rg3915@yahoo.com.br>
2017-10-10 15:08:13 -0300

    IBGE Names

---
+++ ibge_names.py
@@ -0,0 +1,51 @@
+import requests
+
+
+def _parse_response(json_response):
+    if isinstance(json_response, dict):
+        freq = None
+        name = None
+        alternatives = None
+    else:
+        name = json_response[0]['nome']
+        freq = json_response[0]['freq']
+        alternatives = json_response[0]['nomes'].split(',')
+
+    return name, freq, alternatives
+
+
+def classify_by_sex(name):
+    ''' Classify a name by sex using IBGE Nomes API '''
+    # Obtenção
+    url_male = 'https://servicodados.ibge.gov.br/api/v1/censos/nomes/basica?nome={}&sexo=m'
+    url_female = 'https://servicodados.ibge.gov.br/api/v1/censos/nomes/basica?nome={}&sexo=f'
+    response_male = requests.get(url_male.format(name))
+    response_female = requests.get(url_female.format(name))
+
+    # Extração
+    json_male = response_male.json()
+    json_female = response_female.json()
+
+    male = _parse_response(json_male)
+    female = _parse_response(json_female)
+
+    if male[0] is not None:
+        name = male[0]
+    else:
+        name = female[0]
+
+    alternatives = []
+    if male[2] is not None:
+        alternatives.extend(male[2])
+    if female[2] is not None:
+        alternatives.extend(female[2])
+
+    return {
+        'name': name,
+        'male': male[1],
+        'female': female[1],
+        'alternatives': alternatives,
+    }
+
+
+print(classify_by_sex('Regis'))
